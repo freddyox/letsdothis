0db554e5002b8141a4193c19217e01b28de3a7d8
Freddy <obrecht@phys.uconn.edu>
2019-01-25 10:52:40 -0500

    Added functioning webapp

---
+++ webapp/app/views.py
@@ -0,0 +1,17 @@
+from flask import render_template, url_for
+from app import app
+from flask import request
+import pandas as pd
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
