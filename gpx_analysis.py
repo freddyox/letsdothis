692939a6c56263312ea7d5e2e0105a6b2d3c0e94
Freddy <obrecht@phys.uconn.edu>
2019-03-04 08:37:45 -0500

    gpx

--- gpx_analysis.py
+++ gpx_analysis.py
@@ -7,30 +7,58 @@ import os.path
 import csv
 import math
 
+#################################################################
+# Obrecht - 1/20/2019 - Insight Data Science
+# Goal: Read a CSV table, find/open GPS files, and extract physical
+# quantities for each file.  We also want to map each GPS file to
+# runbritain's unique indentifier.
+#################################################################
+
+# This variable does not change in the code; all
+# important outputs go here for subsequent analysis
 FGLOBAL = open("all_elevations.csv", "w")
 
 ##################################################
 # Some book-keeping
-#
-km2mi = 0.621371
 dir = '/home/freddy/insight/data/'
 ext = 'gpx_data/'
-conversion={'marathon'         : 26.219,
-            'half_marathon'    : 13.109,
-            'quarter_marathon' :  6.555,
-            '6hours'           : -1.000,
-            'ultra_marathon'   : -1.000
+km2mi = 0.621371
+conversion = {'marathon'         : 26.219,
+              'half_marathon'    : 13.109,
+              'quarter_marathon' :  6.555,
+              '6hours'           : -1.000, # ignore
+              'ultra_marathon'   : -1.000  # ignore
 }
 
+##################################################
+# Accepts a URL, cuts it up to find an identifier
+# that is unique to runbritain.com (where I'm scraping)
 def cut_up_url(url):
+    """ 
+    Input:  A URL string consistent w/ runbritain.com. Here is an ex)
+    https://www.runbritainrankings.com/results/results.aspx?meetingid=201320&pagenum=1
+    
+    Output: Unique ID int for a particular race, which is how
+    I cross-reference GPS info with race info
+    """ 
     flag = 'meetingid='
-    start = url.find(flag)+len(flag) 
+    start = url.find(flag) + len(flag) 
     end   = url.find('&')
     substr = url[start:end]
-    return int(substr)
+    return int(substr) 
 
+##################################################
+# Open the GPS file, calculate some quantities,
+# and spit out the result to a text file which is
+# used in subsequent analyses.
 def get_gpx_info(filename, meeting_id):
-    name = dir+ext+filename
+    """ 
+    Input:  GPS filename (string) and it's corresponding unique
+    identifier (int), which is labeled as meeting_id
+    
+    Output: A list of important features for each GPS file, all floats
+    """ 
+    name = dir + ext + filename
     elev = []
     try:
         gpx_file = open(name,'r')
@@ -56,7 +84,7 @@ def get_gpx_info(filename, meeting_id):
     sum += elev[len(elev)-1]         # Here is last point
     mean_elevation = sum / len(elev) # Normalize
 
-    # Let's find the Standard Deviation
+    # Let's find the Standard Deviation and write the results:
     sum2=0.0
     bin=0
     for i in elev:
@@ -66,10 +94,11 @@ def get_gpx_info(filename, meeting_id):
 
     sum2norm = sum2 / len(elev) # variance
     sigma = math.sqrt(sum2norm) # std dev
-    features = [sum_up, sum_down, mean_elevation, sigma]
-    return features
+    return [sum_up, sum_down, mean_elevation, sigma]
 
 ################################################################################
+# Main Driver Script - nothing fancy as I only need to extract a few quantities
+# from each file, then move on to deeper analyses within a different script. 
 gpx_info = dir + 'gpx_discover_new.csv'
 df = pd.read_csv(gpx_info)
 cols = list(df.columns)
@@ -96,6 +125,7 @@ for index, row in df.iterrows():
     print( '\t %d \t %1.3f \t %1.3f \t %1.3f \t %1.3f \n' %
            (meeting_id, features[0], features[1], features[2], features[3]) )
 
+# Make an easier to read CSV table so I do not have to rely on hand-me-downs
 f = open("gpx_elevation_map_new.csv", "w")
 for idx, val in enumerate(ID):
     f.write('%d,%1.3f,%1.3f,%1.3f,%1.3f\n' %
